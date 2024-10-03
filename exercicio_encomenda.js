ac83b9842adf8fc00aa413430209c19d08c28e3f
Renata Riboli <renata_riboli@hotmail.com>
2024-10-03 11:17:51 -0300

    5 segundos

--- exercicio_encomenda.js
+++ exercicio_encomenda.js
@@ -1,34 +1,40 @@
-console.log('Faça seu pedido')
-   let confirmacaoDoPedido = new Promise((resolve, reject) => {
-        console.log('Pedido confirmado!')
+console.log('Faça seu pedido');
 
-        setTimeout(() => {
-            let sucesso = true;
-            if(sucesso) { 
-                return resolve([{descrição: 'tomate'}, {descrição: 'cebola'}, {descrição: 'batata'}])
-            }else{
-                return reject('Ocorreu um erro ao fazer o pedido.')
-            }
-        }, 3000)
-   });
+setTimeout(() => {
+    let confirmacaoDoPedido = new Promise((resolve, reject) => {
+        console.log('Pedido confirmado! Confirme se esses são seus itens:\nTomate\nCebola\nBatata.');
+
+        // Simulando o sucesso do pedido
+        let sucesso = true;
+        if (sucesso) {
+            return resolve([{ descrição: 'tomate' }, { descrição: 'cebola' }, { descrição: 'batata' }]);
+        } else {
+            return reject('Ocorreu um erro ao fazer o pedido.');
+        }
+    });
+
+    confirmacaoDoPedido
+        .then((resposta) => {
+            return fazerOPagamento(resposta);
+        })
+        .then((pagamento) => {
+            console.log(pagamento);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+}, 5000);
 
 let fazerOPagamento = (pagamento) => new Promise((resolve, reject) => {
-    console.log('Escolha uma forma de pagamento: cartão de crédito, pix ou cartão de débito.')
-    
+    console.log('Escolha uma forma de pagamento: cartão de crédito, pix ou cartão de débito.');
+
     setTimeout(() => {
         let sucesso = true;
         if (sucesso) {
-            return resolve('Pagamento efetuado. Aguardando envio.')
-        }else{
-            return reject('O pagamento não foi efetuado. Verifique o seu método de pagamento.')
+            return resolve('Pagamento efetuado. Aguardando envio.');
+        } else {
+            return reject('O pagamento não foi efetuado. Verifique o seu método de pagamento.');
         }
-    }, 3000)
-})
-confirmacaoDoPedido
-    .then((resposta) => {
-        return fazerOPagamento(resposta)
-})
-    .then((pagamento) => {
-        console.log(pagamento)
-})
-    
+    }, 3000);
+});
